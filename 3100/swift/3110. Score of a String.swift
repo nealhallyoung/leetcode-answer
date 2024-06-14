b772ede0f321b05fb0df2448d3b5765ec3278db5
hall <ohjunk2001@outlook.com>
2024-06-14 18:09:01 +0800

    Update 3110. Score of a String.swift

--- 3100/swift/3110. Score of a String.swift
+++ 3100/swift/3110. Score of a String.swift
@@ -1,10 +1,10 @@
-class Solution {
-    func scoreOfString(_ s: String) -> Int {
-        var res = 0
-        for i in 0..<s.count - 1 {
-            let char1 = s[s.index(s.startIndex, offsetBy: i)].asciiValue!
-            let char2 = s[s.index(s.startIndex, offsetBy: i + 1)].asciiValue!
-            res += abs(Int(char1) - Int(char2))
+class Solution{
+    func scoreOfString(_ s: String) -> Int{
+        var res=0
+        for i in 1..<s.count{
+            let char1=s[s.index(s.startIndex, offsetBy: i-1)].asciiValue!
+            let char2=s[s.index(s.startIndex, offsetBy: i)].asciiValue!
+            res+=abs(Int(char1)-Int(char2))
         }
         return res
     }
