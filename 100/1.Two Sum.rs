bf37af0971632d1c4df68b4b06a0e07a2a75e553
hall <ohjunk2001@outlook.com>
2024-06-14 16:21:13 +0800

    Create 1.Two Sum.rs

---
+++ 100/1.Two Sum.rs
@@ -0,0 +1,21 @@
+struct Solution{
+
+}
+fn main() {
+    let a=vec![3,2,4];
+    let result=Solution::two_sum(a, 6);
+    println!("{:?}",result);
+}
+//1
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() {
+            for j in i+1..nums.len() {
+                if nums[i]+nums[j]==target {
+                    return vec![i as i32,j as i32];
+                }
+            }
+        }
+        unreachable!()
+    }
+}
